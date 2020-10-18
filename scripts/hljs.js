f6b101f995110b7b1bc63b53223546405ded42eb
wood3n <wood3n@users.noreply.github.com>
2020-10-18 15:40:16 +0000

    deploy: 84a23c6991e79405374e20583fe40079044a4f41

--- scripts/hljs.js
+++ scripts/hljs.js
@@ -1,1 +1,1 @@
-document.addEventListener("DOMContentLoaded",e=>{document.querySelectorAll(".__dumi-default-code-block pre").forEach(e=>{hljs.highlightBlock(e)})});
+document.addEventListener("DOMContentLoaded",(e=>{document.querySelectorAll(".__dumi-default-code-block pre").forEach((e=>{hljs.highlightBlock(e)}))}));
